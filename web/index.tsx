8d9412a03fcc35ed91b39ddc82e56f522dab06f4
Nicolás Gargano <nicolasgargano@gmail.com>
2021-10-11 20:43:07 -0300

    chore: add windi

--- web/index.tsx
+++ web/index.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.sass"
 import {App} from "./src/App"
+import "virtual:windi.css"
 
 ReactDOM.render(
   <React.StrictMode>
